53b1f9771c38803731035086e1e1ceaa21960847
miketillman95 <matillman95@gmail.com>
2022-05-06 18:45:22 +0100

    product page complete

--- ecommerce/components/Product.jsx
+++ ecommerce/components/Product.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 import link from 'next/link'
 
 import {urlFor} from '../lib/client'
+import Link from 'next/link'
 
 const Product = ({product: {image, name, slug, price}}) => {
   // importing the props from the schema object that will pass in the data
   return (
-    <div>Product</div>
+    <div>
+      <Link href= {`/product/${slug.current}`}>
+        <div className="product-card">
+          <img 
+            src= {urlFor(image && image[0])}
+            width = {250}
+            height = {250}
+            className= "product-image"
+            />
+            <p className='= "product-name'>{name}</p>
+            <p className='= "product-price'>${price}</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
