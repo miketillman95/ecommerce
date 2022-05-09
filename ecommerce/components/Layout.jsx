31ff4d98d7fa94613cc80d8702dd39bde149ec40
miketillman95 <matillman95@gmail.com>
2022-05-09 19:28:47 +0100

    page setup

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import Footer from './Footer';
+import NavBar from './NavBar';
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>JS Mastery Store</title>
+      </Head>
+      <header>
+        hey
+        <NavBar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
