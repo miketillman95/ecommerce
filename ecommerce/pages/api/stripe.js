1d4f5b99ec6f9daac5e9d369ba429509cfb4ad65
mike <matillman95@gmail.com>
2022-07-06 19:11:36 +0100

    setting up params

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -6,18 +6,21 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
+      const params = {
+
         line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
+            {
+              // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+              price: '{{PRICE_ID}}',
+              quantity: 1,
+            },
+          ],
+          mode: 'payment',
+          success_url: `${req.headers.origin}/?success=true`,
+          cancel_url: `${req.headers.origin}/?canceled=true`,
+      }
+      
+      const session = await stripe.checkout.sessions.create(params);
       res.redirect(303, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
